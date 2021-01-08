8fd3a7ccf50f46e6fc3b0ef6acdedb2c4f9c4ac1
Brian J. Miller <brian.miller@rusticisoftware.com>
2021-01-08 16:54:28 -0600

    cts-service: add LRS proxy, add version to apis resources

--- cts/service/index.js
+++ cts/service/index.js
@@ -1,17 +1,17 @@
 /*
-Copyright 2020 Rustici Software
+    Copyright 2020 Rustici Software
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 "use strict";
 
@@ -34,12 +34,33 @@ const provision = async () => {
         }
     );
 
+    server.app = {
+        player: {
+        }
+    };
+
     await server.register(H2o2);
     await server.register(Inert);
 
     server.route(routes);
 
     await server.start();
+
+    process.on(
+        "SIGINT",
+        () => {
+            try {
+                await server.stop({timeout: 10000});
+
+                console.log(`Catapult CTS service stopped`);
+                process.exit(0);
+            }
+            catch (ex) {
+                console.log("Catapult CTS service failed to stop gracefully: terminating the process", ex);
+                process.exit(1);
+            }
+        }
+    );
 
     console.log("Catapult CTS service running on %s", server.info.uri);
 };

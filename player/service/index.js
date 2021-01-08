af8fafd60014d6e9accbfa6492faa8f55628d197
Brian J. Miller <brian.miller@rusticisoftware.com>
2021-01-08 16:56:25 -0600

    player: update resources, add SIGINT handling

--- player/service/index.js
+++ player/service/index.js
@@ -1,17 +1,17 @@
 /*
-Copyright 2020 Rustici Software
+    Copyright 2020 Rustici Software
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 "use strict";
 
@@ -45,6 +45,22 @@ const provision = async () => {
     server.route(routes);
 
     await server.start();
+
+    process.on(
+        "SIGINT",
+        async () => {
+            try {
+                await server.stop({timeout: 10000});
+
+                console.log(`Catapult player service stopped`);
+                process.exit(0);
+            }
+            catch (ex) {
+                console.log("Catapult player service failed to stop gracefully: terminating the process", ex);
+                process.exit(1);
+            }
+        }
+    );
 
     console.log("Catapult player service running on %s", server.info.uri);
 };
--- player/service/lib/routes.js
+++ player/service/lib/routes.js
@@ -1,17 +1,17 @@
 /*
-Copyright 2020 Rustici Software
+    Copyright 2020 Rustici Software
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 "use strict";
 
@@ -32,7 +32,6 @@ module.exports = [
     // proxy the underlying LRS resources
     //
     {
-        // method: "*",
         method: [
             "GET",
             "POST",
@@ -94,7 +93,13 @@ module.exports = [
 
                     response.code(res.statusCode);
                     response.message(res.statusMessage);
-                    response.headers = res.headers;
+
+                    for (const [k, v] of Object.entries(res.headers)) {
+                        if (k.toLowerCase() !== "transfer-encoding") {
+                            response.header(k, v);
+                        }
+                    }
+
 
                     // clean up the original response
                     res.destroy();
@@ -124,22 +129,15 @@ module.exports = [
 
     {
         method: "GET",
-        path: "/api/ping",
+        path: "/api/v1/ping",
         handler: (req, h) => ({
             ok: true
         })
     },
 
     {
-        method: "POST",
-        path: "/api/content",
-        handler: (req, h) => {
-        }
-    },
-
-    {
         method: "GET",
-        path: "/api/course-structure",
+        path: "/api/v1/course/{courseId}",
         handler: (req, h) => ({
             course: {
                 id: "http://example.org...",
@@ -153,8 +151,22 @@ module.exports = [
     },
 
     {
+        method: "POST",
+        path: "/api/v1/course/{courseId}",
+        handler: (req, h) => {
+        }
+    },
+
+    {
+        method: "DELETE",
+        path: "/api/v1/course/{courseId}",
+        handler: (req, h) => {
+        }
+    },
+
+    {
         method: "GET",
-        path: "/api/launch-url",
+        path: "/api/v1/course/{courseId}/launch-url/{auIndex}",
         handler: async (req, h) => {
             console.log("get launchUrl");
 
